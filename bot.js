28a2c3d05a58048964c7862498ceb86f2851e25d
avarento <siede@protonmail.com>
2023-09-07 00:59:34 -0300

    (reading 'startsWith')

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ async function start_bot() {
         let info = await extract(pool);
         console.log(info)
        
-        if (info.msg.startsWith("#") && IaN(info.msg.replace("#", "")) === true) {
+        if (info?.msg?.startsWith("#") && IaN(info.msg.replace("#", "")) === true) {
             pesquisaScherer(info.msg.replace("#", "")).then(async (pesquisa) => {
                 console.log("Scherer: ", pesquisa.scherer, "Status: ", pesquisa.status)
 
