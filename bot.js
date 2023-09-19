b5e18e92c9ce3a203dafd5edb996b1db0f956a3c
Gustavo Siede <124945658+avarento@users.noreply.github.com>
2023-09-19 13:32:41 -0300

    "teste sem image" Update bot.js

--- bot.js
+++ bot.js
@@ -19,10 +19,10 @@ async function start_bot() {
                 console.log("Scherer: ", pesquisa.scherer, "Status: ", pesquisa.status)
 
                 if (pesquisa.status === "válido") {
-                    const img = await get_img_url(pesquisa.imgURL);
+                    //const img = await get_img_url(pesquisa.imgURL);
                     let mensagem = `*_Cod Scherer:_* ${pesquisa.scherer} \n\n*_Código da peça:_* ${pesquisa.codigo} \n\n*_Descrição:_* ${pesquisa.descricao} 
                             \n\n`
-                    await waSocket.sendMessage(info.id, {image: img, caption: mensagem})
+                    await waSocket.sendMessage(info.id, {text: mensagem})
 
                 } else if (pesquisa.status === "inválido") {
                     let mensagem = `O código scherer *${pesquisa.scherer}* pode não existir, verifique novamente.`
