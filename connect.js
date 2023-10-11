d0b24bbe71245bc2a3a6175111008fecc96627f3
Gustavo Siede <124945658+avarento@users.noreply.github.com>
2023-10-11 14:00:31 -0300

    tentando isolar o erro com try catch

--- connect.js
+++ connect.js
@@ -2,31 +2,36 @@ const { default:  MessageType, MessageOptions, Mimetype, makeWASocket, Browsers,
 
 
 async function connect() {
-    const { state, saveCreds } = await useMultiFileAuthState(`wpp_auth`)
-    const waSocket = makeWASocket({
-        printQRInTerminal: true,
-        syncFullHistory: false,
-        auth: state,
-    });
-
-    waSocket.ev.on('connection.update', (update) => {
-        const { connection, lastDisconnect } = update;
-        
-        if (connection === 'close') {
-            const shouldReconnect = lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut;
-
-            if (shouldReconnect) {
-                connect();
-            }
-        }
-
-    }); 
-
-    waSocket.ev.on('creds.update', saveCreds)
-    
-
-    return waSocket;
+  try {
+      const { state, saveCreds } = await useMultiFileAuthState(`wpp_auth`)
+      const waSocket = makeWASocket({
+          printQRInTerminal: true,
+          syncFullHistory: false,
+          auth: state,
+      });
+  
+      waSocket.ev.on('connection.update', (update) => {
+          const { connection, lastDisconnect } = update;
+          
+          if (connection === 'close') {
+              const shouldReconnect = lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut;
+  
+              if (shouldReconnect) {
+                  connect();
+              }
+          }
+  
+      }); 
+  
+      waSocket.ev.on('creds.update', saveCreds)
+      
+  
+      return waSocket;
+  } catch (err) {
+    console.log("ERRO EM CONNECT - SITUAÇÃO: ", err);
+  }
 }
+
 
 module.exports = {
     connect: connect
